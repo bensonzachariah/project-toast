affaa3a55ec74670fc12e89fc95446f842542e69
benson.zachariah <benson_zachariah@yahoo.com>
2024-09-17 07:41:49 +0530

    custom hook - useEscapeKey

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,4 +1,5 @@
 import React from "react";
+import useEscapeKey from "../../hooks/useEscapeKey/useEscapeKey";
 export const ToastContext = React.createContext();
 
 function ToastProvider({ children }) {
@@ -17,16 +18,10 @@ function ToastProvider({ children }) {
     setToasts(nextToasts);
   };
 
-  React.useEffect(() => {
-    const handleEscape = (e) => {
-      if (e.code === "Escape") {
-        setToasts([]);
-      }
-    };
-    window.addEventListener("keydown", handleEscape);
-    return () => window.removeEventListener("keydown", handleEscape);
+  useEscapeKey(() => {
+    setToasts([]);
   });
-
+  
   return (
     <ToastContext.Provider value={{ toasts, dismissToast, addToast }}>
       {children}
